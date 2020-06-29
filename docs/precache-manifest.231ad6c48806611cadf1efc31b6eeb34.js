cf0d513a2a7a43461a92e2f43d88ee304828d94a
Jorge Caballero Barranco <oreyabak@gmail.com>
2020-06-29 16:33:33 +0200

    docs agregado.

---
+++ docs/precache-manifest.231ad6c48806611cadf1efc31b6eeb34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6a65a79d36c5662ff75bbf3dab3ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c74701f931241bb14fa6",
+    "url": "/static/css/main.b4f58d22.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c74701f931241bb14fa6",
+    "url": "/static/js/main.d66ec548.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
